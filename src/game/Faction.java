15badf6a73f5fb445b2ac5f389fa68134e83a9bd
joulupunikki <joulupunikki@gmail.communist.invalid>
2014-02-15 08:41:44 +0200

    Implement unit building on a branch implement build button on planet window halfway, must set planet selected on planet_list and city selected on city_table. Include Faction.java should have been included two commits previous.

--- src/game/Faction.java
+++ src/game/Faction.java
@@ -4,18 +4,54 @@
  */
 package game;
 
-import galaxyreader.Structure;
-import galaxyreader.Unit;
+import java.io.Serializable;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
+import util.C;
 
 /**
+ * Contains per faction data: firebirds, messages, buildable units.
  *
  * @author joulupunikki
  */
-public class Faction {
+public class Faction implements Serializable {
 
-    List<Unit> units;
-    List<Unit> unmoved_units;
-    List<Structure> cities;
-    // Techs      
+//    List<Unit> units;
+//    List<Unit> unmoved_units;
+//    List<Structure> cities;
+    int firebirds = C.STARTING_FIREBIRDS;
+    List<Message> messages = new LinkedList<>();
+
+    public Faction() {
+    }
+
+    public void addMessage(Message m) {
+        messages.add(m);
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void deleteOldMessages(int year) {
+        ListIterator<Message> iter = messages.listIterator();
+        Message m = null;
+        while (iter.hasNext()) {
+            m = iter.next();
+            if (year > m.year) {
+                iter.remove();
+            }
+        }
+
+    }
+
+    public static Faction[] createFactions() {
+        Faction[] factions = new Faction[C.NR_FACTIONS];
+        for (int i = 0; i < factions.length; i++) {
+            factions[i] = new Faction();
+
+        }
+        return factions;
+    }
 }
